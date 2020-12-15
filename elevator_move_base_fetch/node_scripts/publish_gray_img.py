a5676650b888aa95fc6d2f601a0b3cc436d5d475
obinata <mqcmd196@hotmail.co.jp>
2020-12-15 20:52:08 +0900

    remove unnecessary files and change ns for camera info

--- elevator_move_base_fetch/node_scripts/publish_gray_img.py
+++ elevator_move_base_fetch/node_scripts/publish_gray_img.py
@@ -5,19 +5,25 @@ from cv_bridge import CvBridge
 import cv2
 
 from sensor_msgs.msg import Image
+from sensor_msgs.msg import CameraInfo
 
 class RGB2Gray:
     def __init__(self):
         rospy.init_node('rgb2gray', anonymous=True)
-        self.sub = rospy.Subscriber('/head_camera/rgb/image_rect_color', Image, self.callback)
-        self.pub = rospy.Publisher('/head_camera/image_rect', Image, queue_size=1)
+        self.img_sub = rospy.Subscriber('/head_camera/rgb/image_rect_color', Image, self.img_callback)
+        self.info_sub = rospy.Subscriber('/head_camera/rgb/camera_info', CameraInfo, self.info_callback)
+        self.img_pub = rospy.Publisher('/head_camera/mono/image_rect', Image, queue_size=1)
+        self.info_pub = rospy.Publisher('/head_camera/mono/image_rect', CameraInfo, queue_size=1)
         self.bridge = CvBridge()
 
-    def callback(self, data):
+    def img_callback(self, data):
         cv2_img = self.bridge.imgmsg_to_cv2(data, "bgr8")
         gray_img = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2GRAY)
         gray_msg = self.bridge.cv2_to_imgmsg(gray_img, "mono8")
         self.pub.publish(gray_msg)
+
+    def info_callback(self, data):
+        self.pub.publish(data)
 
 if __name__ == '__main__':
     try:
