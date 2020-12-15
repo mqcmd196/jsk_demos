33a25db7ad63f9e167794388108ec7c1f82b9c4d
obinata <mqcmd196@hotmail.co.jp>
2020-12-15 17:43:50 +0900

    remove apps and add make /head_camera/image_rect

---
+++ elevator_move_base_fetch/node_scripts/publish_gray_img.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import rospy
+from cv_bridge import CvBridge
+import cv2
+
+from sensor_msgs.msg import Image
+
+class RGB2Gray:
+    def __init__(self):
+        rospy.init_node('rgb2gray', anonymous=True)
+        self.sub = rospy.Subscriber('/head_camera/rgb/image_rect_color', Image, self.callback)
+        self.pub = rospy.Publisher('/head_camera/image_rect', Image, queue_size=1)
+        self.bridge = CvBridge()
+
+    def callback(self, data):
+        cv2_img = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+        gray_img = self.cv2.cvtColor(cv2_img, cv2.COLOR_BGR2GRAY)
+        gray_msg = self.bridge.cv2_to_imgmsg(gray_img, "mono16")
+        self.pub.publish(gray_msg)
+
+if __name__ == '__main__':
+    try:
+        node = RGB2Gray()
+    except rospy.ROSInterruptException:
+        pass
