4d8d88e6826f0d37197c8b7ddb9bbadd0f665e92
Yoshiki Obinata <obinata@jsk.imi.i.u-tokyo.ac.jp>
2020-12-16 14:19:33 +0900

    fix bug of camera_info

--- elevator_move_base_fetch/node_scripts/publish_gray_img.py
+++ elevator_move_base_fetch/node_scripts/publish_gray_img.py
@@ -4,15 +4,12 @@ from cv_bridge import CvBridge
 import cv2
 
 from sensor_msgs.msg import Image
-from sensor_msgs.msg import CameraInfo
 
 class RGB2Gray:
     def __init__(self):
         rospy.init_node('rgb2gray', anonymous=True)
         self.img_sub = rospy.Subscriber('/head_camera/rgb/image_rect_color', Image, self.img_callback)
-        self.info_sub = rospy.Subscriber('/head_camera/rgb/camera_info', CameraInfo, self.info_callback)
         self.img_pub = rospy.Publisher('/head_camera/mono/image_rect', Image, queue_size=1)
-        # self.info_pub = rospy.Publisher('/head_camera/mono/camera_info', CameraInfo, queue_size=1)
         self.bridge = CvBridge()
 
     def img_callback(self, data):
@@ -21,9 +18,6 @@ class RGB2Gray:
         gray_msg = self.bridge.cv2_to_imgmsg(gray_img, "mono8")
         gray_msg.header = data.header
         self.img_pub.publish(gray_msg)
-
-    # def info_callback(self, data):
-    #     self.info_pub.publish(data)
 
 if __name__ == '__main__':
     try:
